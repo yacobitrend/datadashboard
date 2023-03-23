f7c0c0f2b0145702e834848dc9379b96569957e3
yacobitrend <128684968+yacobitrend@users.noreply.github.com>
2023-03-23 17:33:12 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,12 +2,6 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-##st.set_page_config(
-##    page_title="Home Marketplace Metrics",
-##    page_icon="👋",)
-
-##st.write("# Online Performance Dashboard 👋")
-
 st.title('Itrend Solution')
 
 import streamlit as st
@@ -38,12 +32,13 @@ data = pd.read_csv('pages/sales.csv',sep=',', index_col=False)
 
 #ASIN Details..
 asin_data = pd.read_csv('pages/ASIN-TIER.csv',sep=',', index_col=False)
-=======
+
+
 data = pd.read_csv('datadashboard/sales.csv',sep=',', index_col=False)
 
 #ASIN Details..
 asin_data = pd.read_csv('datadashboard/ASIN-TIER.csv',sep=',', index_col=False)
->>>>>>> fa4ae9781c6cdb2ae73af28e2db8f3f9daffb2e6
+
 
 data['Sales'] = pd.to_numeric(data['Sales'], errors='coerce')
 
@@ -335,23 +330,3 @@ with col9:
 
 
 
-##row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.columns((.1, 2.3, .1, 1.3, .1))
-##
-####with row0_1:
-####    st.title('Online Performance Dashboard')
-##    
-##with row0_2:
-##    st.text("")
-##    st.subheader('Streamlit App by [Tim Denzler](https://www.linkedin.com/in/tim-denzler/)')
-
-
-
-##row3_spacer1, row3_1, row3_spacer2 = st.columns((.1, 3.2, .1))
-##
-##
-##with row3_1:
-##    
-##    st.markdown("Hello there! Have you ever spent your weekend watching the German Bundesliga and had your friends complain about how 'players definitely used to run more' ? However, you did not want to start an argument because you did not have any stats at hand? Well, this interactive application containing Bundesliga data from season 2013/2014 to season 2019/2020 allows you to discover just that! If you're on a mobile device, I would recommend switching over to landscape for viewing ease.")
-##    st.markdown("You can find the source code in the [BuLiAn GitHub Repository](https://github.com/tdenzl/BuLiAn)")
-##    st.markdown("If you are interested in how this app was developed check out my [Medium article](https://tim-denzler.medium.com/is-bayern-m%C3%BCnchen-the-laziest-team-in-the-german-bundesliga-770cfbd989c7)")
-##
